7f79681b2316787489f86fbe781bf8b7399ac0a3
minga <100984882+Khaki1335@users.noreply.github.com>
2025-07-03 07:29:41 +0900

    clean コマンドで問答無用に消すように変更

--- bin/clean.bat
+++ bin/clean.bat
@@ -7,20 +7,18 @@ set "DOTFILES_DIR=%~dp0..\files"
 pushd "%DOTFILES_DIR%"
 
 for %%a in (.??*) do (
-  set "link=%USERPROFILE%\%%a"
-  dir /l /a:al "!link!" 2>nul | findstr /i "SYMLINK" >nul 
-  if !errorlevel! == 0 (
-    del /q "!link!"
-    echo Deleted "!link!".
+  set "target=%USERPROFILE%\%%a"
+  if exist "!target!" (
+    del /q "!target!"
+    echo Deleted "!target!".
   )
 )
 
 for /d %%a in (.??*) do (
-  set "link=%USERPROFILE%\%%a"
-  dir /l /a "%USERPROFILE%" 2>nul | findstr /i "SYMLINKD" | findstr "%%a" >nul
-  if !errorlevel! == 0 (
-    rd "!link!"
-    echo Deleted "!link!".
+  set "target=%USERPROFILE%\%%a"
+  if exist "!target!" (
+    rd "!target!"
+    echo Deleted "!target!".
   )
 )
 
--- bin/clean.sh
+++ bin/clean.sh
@@ -4,12 +4,13 @@ set -eu
 DOTFILES_DIR="$(cd "$(dirname "$0")/../files" && pwd)"
 
 for dotfile in "$DOTFILES_DIR"/.??*; do
-  link="$HOME/$(basename $dotfile)"
-  readlink $link > /dev/null
-  result=$?
-
-  if [ $result -eq 0 ]; then
-    rm $link
-    echo Deleted "$link".
+  target="$HOME/$(basename $dotfile)"
+  if [ -f "$target" ]; then
+    rm $target
+    echo Deleted "$target".
+  elif [ -d "$target" ]; then
+    rm -rf $target
+    echo Deleted "$target".
   fi
+
 done
